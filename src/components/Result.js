6a2384359138520aff89b93df4fa20124b3994a1
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-25 11:59:44 -0500

    aligned text in results box

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,10 +3,12 @@ import React from 'react'
 function Result ({finalResult}) {
     return (
         <div className='results-div' style={ {backgroundImage: 'url(' + finalResult.image + ')',backgroundSize: '100%'}}>
-        <h2>Your Result:</h2>
-        <h3>{finalResult.result}</h3>
-        <p>{finalResult.text}</p>
-        <p className="img-credit">Photo by <a target="_blank" href={"https://unsplash.com/@"+finalResult.credit}> @{finalResult.credit} for Unsplash</a></p>
+        <div className='results-textbox'>
+            <span>Your Result:</span>
+            <h2>{finalResult.result}</h2>
+            <p>{finalResult.text}</p>
+            <p className="img-credit">Photo by <a target="_blank" href={"https://unsplash.com/@"+finalResult.credit}> @{finalResult.credit} for Unsplash</a></p>
+        </div>
         </div>
     )
 
